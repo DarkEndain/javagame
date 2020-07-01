40218df95921501b5d17a877aa795eec88fbf31b
Ashuriii <67146881+Ashuriii@users.noreply.github.com>
2020-07-01 10:52:10 +0200

    Update items.java

--- src/javagame/items.java
+++ src/javagame/items.java
@@ -4,9 +4,8 @@ public class items {
 
 	private int id;
 	private String name;
-	private String beschreibung;
-	private int anzahl;
-	private boolean bekommen;
-	private boolean istQuestItem;
+	private String description;
+	private boolean inInventory;
+	private boolean usable;
 	
 }
