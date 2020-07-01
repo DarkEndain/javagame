d061e434037d6f497668c9e110dfc8639a84b3d6
Dominik Färber <dominik.faerber.dfr@gmail.com>
2020-07-01 10:38:16 +0200

    Create items.java
    Grundgerüst für Items erstellt

---
+++ src/javagame/items.java
@@ -0,0 +1,12 @@
+package javagame;
+
+public class items {
+
+	private int id;
+	private String name;
+	private String beschreibung;
+	private int anzahl;
+	private boolean bekommen;
+	private boolean istQuestItem;
+	
+}
