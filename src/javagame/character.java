66867fe8a71fb4c2f4485fa5e3d3c0b2e9a81583
Dominik Färber <dominik.faerber.dfr@gmail.com>
2020-07-08 09:08:27 +0200

    Klassen hinzugefügt
    Habe die Klasse Charakter und Consumables hinzugefügt
    
    TODO:
    In der Klasse Charakter müssen noch die Verknüpfungen zum Inventar und zur Armor angelegt werden

---
+++ src/javagame/character.java
@@ -0,0 +1,26 @@
+package javagame;
+
+public class character {
+
+	private String name;
+	private int hp;
+	private int mp;
+	private int baseStrength;
+	private int bonusStrenght;
+	private int baseIntelect;
+	private int bonusIntelect;
+	private int baseAgility;
+	private int bonusAgility;
+	private int baseLuck;
+	private int bonusLuck;
+	private int armor;
+	private int resistence;
+	private int dodge;
+	private int initiative;
+	
+	/*
+	 * Todo
+	 * Verknüpfung zum UNventar und zur Armory müssen noch hinzugefügt werden
+	 */
+	
+}
---
+++ src/javagame/consumables.java
@@ -0,0 +1,12 @@
+package javagame;
+
+public class consumables extends items{
+	
+	private int id;
+	private String name;
+	private String description;
+	private int amount;
+	private boolean inInventory;
+	private boolean usable;
+
+}
